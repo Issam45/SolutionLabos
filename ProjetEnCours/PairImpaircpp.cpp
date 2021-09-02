1a710e95046cdf3da1c9d07ce8f07482a3578843
Issam45 <89462485+Issam45@users.noreply.github.com>
2021-09-01 21:26:10 -0500

    Update PairImpaircpp.cpp

--- ProjetEnCours/PairImpaircpp.cpp
+++ ProjetEnCours/PairImpaircpp.cpp
@@ -1,10 +1,10 @@
-// But : �crire un programme qui demande un entier � l'utilisateur et affiche s'il s'agit d'un entier pair ou impair
+// But : Écrire un programme qui demande un entier à l'utilisateur et affiche s'il s'agit d'un entier pair ou impair
 // Auteur :	Issam Abdelkrim
 // Date : 2021-08-31
 
 #include <iostream> // Bibliotheque necessaire pour tout les programmes qui dialogues avec l'utilisateur utilisation du --> (cin, cout)
 
-using namespace std; // Inclus le std pour ne pas le remettre � chq fois dans notre code
+using namespace std; // Inclus le std pour ne pas le remettre à chq fois dans notre code
 
 int main()
 	{
@@ -12,7 +12,7 @@ int main()
 
 		setlocale(LC_ALL, "");
 
-		cout << "Ce programme d�termine si votre nombre entier est pair ou impair! \n \n";
+		cout << "Ce programme détermine si votre nombre entier est pair ou impair! \n \n";
 
 		cout << "\t \t \t-------------------------------Exercie 1------------------------------- \n";
 
@@ -64,3 +64,84 @@ int main()
 		return 0;
 	
 	}
+/*
+// But : Écrire un programme qui demande un entier à l'utilisateur et affiche s'il s'agit d'un entier pair ou impair
+// Auteur :	Issam Abdelkrim
+// Date : 2021-08-31
+
+#include <iostream> // Bibliotheque necessaire pour tout les programmes qui dialogues avec l'utilisateur utilisation du --> (cin, cout)
+
+using namespace std; // Inclus le std pour ne pas le remettre à chq fois dans notre code
+
+int main()
+{
+	int entier1;
+
+	setlocale(LC_ALL, "");
+
+	cout << "Ce programme détermine si votre nombre entier est pair ou impair! \n \n";
+
+	cout << "Pour quitter en tout moment rentrer (9999)" << endl;
+
+	cout << "\t \t \t-------------------------------Exercie 1------------------------------- \n";
+
+	cout << "Veuillez entrer un nombre entier: " ;
+
+	cin >> entier1;
+	cout << endl;
+
+	while( entier1 != 9999) 
+	{
+			
+		while (entier1 % 2 == 0 && entier1 != 9999)
+		{
+			cout << "Votre nombre est pair! " << endl;
+
+			system("pause");
+			system("cls");
+
+			cout << "Ce programme détermine si votre nombre entier est pair ou impair! \n \n";
+
+			cout << "Pour quitter en tout moment rentrer (9999)" << endl;
+
+			cout << "\t \t \t-------------------------------Exercie 1------------------------------- \n";
+
+			cout << "Veuillez entrer un nombre entier: ";
+
+			cin >> entier1;
+
+			cout << endl;
+		}
+		cout << endl;
+
+		while (entier1 % 2 != 0 && entier1 != 9999)
+		{
+			cout << "Votre nombre est impair! " << endl;
+
+			system("pause");
+			system("cls");
+
+			cout << "Ce programme détermine si votre nombre entier est pair ou impair! \n \n";
+
+			cout << "Pour quitter en tout moment rentrer (9999)" << endl;
+
+			cout << "\t \t \t-------------------------------Exercie 1------------------------------- \n";
+
+			cout << "Veuillez entrer un nombre entier: ";
+
+			cin >> entier1;
+
+			cout << endl;
+		}
+		
+	}
+	system("pause");
+	return 0;
+
+}
+
+/*
+1. Veuillez rentrer le chiffre 2 --> Le programme doit afficher un message qui mentionne que votre entier est pair!
+
+2. Veuillez rentrer le chiffre 3 --> Le programme doit afficher un message qui mentionne que votre entier est impair!
+*/
